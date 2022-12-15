98fc5b020aab9a0e6a180d59962e8c1f2b474eff
Retro-Hax <daria.dim@retro-hax.net>
2022-12-15 05:13:29 +0100

    Welcome to mmlgb2

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-mmlgb
+mmlgb2
 =====
+
+NOTE: This is a unofficial Continuation of SimonLarsens [mmlgb](https://github.com/SimonLarsen/mmlgb)
 
 A MML parser and GBDK sound driver for the Nintendo Game Boy.
 
-*In development. Most core functionality missing.*
+*In development . Most core functionality missing.*
 
 See [wiki](https://github.com/SimonLarsen/mmlgb/wiki) for instructions.
--- compile.sh
+++ compile.sh
@@ -2,10 +2,10 @@
 
 java -jar parser/MMLGB.jar "$1" driver/song.asm
 
-lcc -c driver/player.asm -o driver/player.o
-lcc -c driver/music.asm -o driver/music.o
-lcc -c driver/song.asm -o driver/song.o
-lcc -c driver/freq.asm -o driver/freq.o
-lcc -c driver/noisefreq.asm -o driver/noisefreq.o
-lcc -c driver/vib.asm -o driver/vib.o
-lcc -Wl-j driver/player.o driver/music.o driver/song.o driver/freq.o driver/noisefreq.o driver/vib.o -o rom.gb
+${GBDK_HOME}bin/lcc -c driver/player.asm -o driver/player.o
+${GBDK_HOME}bin/lcc -c driver/music.asm -o driver/music.o
+${GBDK_HOME}bin/lcc -c driver/song.asm -o driver/song.o
+${GBDK_HOME}bin/lcc -c driver/freq.asm -o driver/freq.o
+${GBDK_HOME}bin/lcc -c driver/noisefreq.asm -o driver/noisefreq.o
+${GBDK_HOME}bin/lcc -c driver/vib.asm -o driver/vib.o
+${GBDK_HOME}bin/lcc -Wl-j driver/player.o driver/music.o driver/song.o driver/freq.o driver/noisefreq.o driver/vib.o -o rom.gb
